69921f545d541d43f5e263ff64366e457024f9ec
Katrina Weers <katrinaweers@gmail.com>
2024-05-04 17:43:19 -0700

    Added podcast and music insert table statements

---
+++ Audiobook_Inserts/Publisher.sql
@@ -0,0 +1,9 @@
+INSERT INTO Publisher(Publisher_ID,Publisher_Name) VALUES
+ (1,'Penguin Random House')
+,(2,'HarperCollins')
+,(3,'Hachette Livre')
+,(4,'Macmillan Publishers')
+,(5,'Simon & Schuster')
+,(6,'Wiley')
+,(7,'Pearson')
+,(8,'Scholastic');
---
+++ Music_Inserts/Record_Label.sql
@@ -0,0 +1,9 @@
+INSERT INTO Record_Label(Label_ID,Label_Name) VALUES
+ (1,'Universal Music Group')
+,(2,'Sony Music Entertainment')
+,(3,'Warner Music Group')
+,(4,'EMI Music')
+,(5,'Capitol Records')
+,(6,'Atlantic Records')
+,(7,'Def Jam Recordings')
+,(8,'Indie/Other');
